667b4e7f72bc55dcfb05ff39b1b0fa740c801b51
kuyluk <kuylukmurat@gmail.com>
2014-01-21 16:03:09 +0200

    Add-on module for Jackson JSON processor which handles Hibernate (http://www.hibernate.org/) datatypes; and specifically aspects of lazy-loading http://wiki.fasterxml.com/JacksonModuleHibernate

---
+++ src/main/java/com/pathsf/example/config/HibernateAwareObjectMapper.java
@@ -0,0 +1,12 @@
+package com.pathsf.example.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate4.Hibernate4Module;
+
+public class HibernateAwareObjectMapper extends ObjectMapper {
+
+	public HibernateAwareObjectMapper() {
+        Hibernate4Module hm = new Hibernate4Module();
+        registerModule(hm);
+    }
+}
